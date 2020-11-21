d9fbe90a51ce0ec1d520863e19da2ebb432ecafb
samsharma97 <Sskakkar1997@gmail.com>
2020-11-21 09:05:11 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/modules.xml

--- src/FoodByVIA/Shared/Menu.java
+++ src/FoodByVIA/Shared/Menu.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Menu extends FoodItem
 {
   ArrayList<FoodItem> Menu;
+
+  public Menu(String name, double price, String description)
+  {
+    super(name, price, description);
+  }
+
   void addFoodItem(FoodItem item)
   {
       Menu.add(item);
