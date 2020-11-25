f7fe3e89e6bbca594ca7e7b207bd80244900f540
Ina2805 <ina.zadravec@live.com>
2020-11-25 18:44:04 +0100

    RMI and Networking AddItem to menu almost finished, waiting for the server side of the rmi

--- src/FoodByVIA/Server/Model/RegisterUser/RegisterUserServerModelImpl.java
+++ src/FoodByVIA/Server/Model/RegisterUser/RegisterUserServerModelImpl.java
@@ -1,8 +1,15 @@
 package FoodByVIA.Server.Model.RegisterUser;
 
+import FoodByVIA.DAO.Persistance.User.UserDaoManager;
 import FoodByVIA.Server.Network.RegisterUserServerImpl;
+import FoodByVIA.Shared.User;
 
 public class RegisterUserServerModelImpl
 {
+  UserDaoManager userDAO = new UserDaoManager();
 
+  public void addUser(User user)
+  {
+    userDAO.createUser(user);
+  }
 }
--- src/FoodByVIA/Server/Network/RegisterUserServerImpl.java
+++ src/FoodByVIA/Server/Network/RegisterUserServerImpl.java
@@ -1,5 +1,6 @@
 package FoodByVIA.Server.Network;
 
+import FoodByVIA.Server.Model.RegisterUser.RegisterUserServerModelImpl;
 import FoodByVIA.Shared.User;
 
 import java.rmi.registry.Registry;
@@ -7,10 +8,10 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class RegisterUserServerImpl
 {
-
+ private RegisterUserServerModelImpl userServerModel = new RegisterUserServerModelImpl();
 
   public void addUser(User user){
-    
+    userServerModel.addUser(user);
   }
 
 }
