21f3ddb496fd1458db6a3161db35894d5b8ef2ac
fatem <fatemapigen@hotmail.com>
2020-11-25 16:11:57 +0100

    Client network register user

---
+++ src/FoodByVIA/Client/Network/NetworkConnection.java
@@ -0,0 +1,45 @@
+package FoodByVIA.Client.Network;
+
+import FoodByVIA.Server.Core.ServerInterface;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class NetworkConnection
+{
+  private ServerInterface serverInterface;
+  private static NetworkConnection instance;
+
+  private NetworkConnection()
+  {
+
+  }
+
+  public static NetworkConnection getInstance()
+  {
+    if(instance == null)
+    {
+      instance = new NetworkConnection();
+    }
+    return instance;
+  }
+
+  public ServerInterface getServerInterface()
+  {
+    if(serverInterface == null)
+    {
+      try
+      {
+        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+        serverInterface = (ServerInterface) registry.lookup("Server");
+      }
+      catch (RemoteException | NotBoundException e)
+      {
+        e.printStackTrace();
+      }
+    }
+    return serverInterface;
+  }
+}
--- src/FoodByVIA/Client/Network/RegisterUser/RegisterUserClientImpl.java
+++ src/FoodByVIA/Client/Network/RegisterUser/RegisterUserClientImpl.java
@@ -1,16 +1,44 @@
 package FoodByVIA.Client.Network.RegisterUser;
 
+import FoodByVIA.Client.Network.NetworkConnection;
 import FoodByVIA.Shared.User;
+
+import java.rmi.RemoteException;
 
 public class RegisterUserClientImpl implements RegisterUserClient
 {
-  @Override public void addUser(User user)
-  {
+  private RegisterUserServer registerUserServer;
+  private NetworkConnection connection;
 
+  public RegisterUserClientImpl(){
+    connection = NetworkConnection.getInstance();
   }
 
   @Override public void startClient()
   {
+    try
+    {
+      //UnicastRemoteObject.exportObject(this, 0);
+      registerUserServer = connection.getServerInterface().getRegisterUserServerImpl;
+      System.out.println("Server Connected");
+    }
+    catch (RemoteException e)
+    {
+      e.printStackTrace();
+    }
+  }
+  @Override public void addUser(User user)
+  {
+    try
+    {
+      registerUserServer.addUser(user);
+    }
+    catch (RemoteException e)
+    {
+      e.printStackTrace();
+    }
 
   }
+
+
 }
