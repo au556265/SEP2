160873e38fdf3c6216ca2c6789f65ff926b8565d
fatem <fatemapigen@hotmail.com>
2020-12-04 00:18:15 +0100

    DineIn

---
+++ src/FoodByVIA/Shared/DineIn.java
@@ -0,0 +1,20 @@
+package FoodByVIA.Shared;
+
+public class DineIn extends Order
+{
+  private String tableID;
+  public DineIn(User customer, double totalPrice, boolean active)
+  {
+    super(customer, totalPrice, active);
+  }
+
+  public void setTableID(String tableID){
+    this.tableID=tableID;
+  }
+
+  public String getTableID()
+  {
+    return tableID;
+  }
+
+}
